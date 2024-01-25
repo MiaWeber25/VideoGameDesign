1a2dc80591fb91b6b6c5cf7e6351dabe8db7bc4f
Mia Weber <miaweber02@gmail.com>
2024-01-25 10:25:25 -0700

    Shared Objects example

---
+++ sharedObjects/fun.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int fun(int x);
+
+#ifdef __cplusplus
+    }
+#endif
---
+++ sharedObjects/sample.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "fun.h"
+
+using namespace std;
+
+
+
+int main() {
+    int x=10;
+    cout << fun(x) << endl;
+    return 0;
+}
