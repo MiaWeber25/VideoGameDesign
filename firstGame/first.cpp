23c42365607af621954bd7ba9e5d28954fce53b3
Mia Weber <miaweber02@gmail.com>
2024-02-20 11:09:10 -0700

    feb20 continue to add physics

--- firstGame/first.cpp
+++ firstGame/first.cpp
@@ -3,8 +3,34 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <map>
 
 using namespace std;
+
+class MediaManager {
+    map<string,SDL_Texture*> textures;
+    SDL_Renderer *ren;
+
+    public:
+    MediaManager(SDL_Renderer *newRen) {
+        ren = newRen;
+    }
+    SDL_Texture * get(string fname) {
+        if (textures.find(fname) == textures.end()) {
+            SDL_Texture *tex;
+            SDL_Surface *bmp = SDL_LoadBMP(fname.c_str()); // "test.bmp" etc.
+            if (!bmp) { // bit map file
+                cerr << "Error reading bit map" << endl;
+                return NULL;
+            }
+            tex = SDL_CreateTextureFromSurface(ren,bmp); // convert the image to the texture (give it to the graphics card)
+            SDL_FreeSurface(bmp); // free up the memory of the surface in the cpu (pushed it to the graphics card in line above)
+            textures[fname]=tex; //set .bmp to texture in map class (assume texture doesn't exist)
+        }
+        return textures[fname];
+    }
+};
+MediaManager *mm = NULL;
 
 class PrimordialGame { // Pure virtual class (all functions don't have defs)
     public:
@@ -17,41 +43,69 @@ class Sprite {
     SDL_Texture *tex;
     SDL_Rect dest;
     SDL_Rect src; // the drawing area I am trying to manipulate
-    int xdir, ydir;
     bool good;
     PrimordialGame *g;
+
+    protected:
+    float x,y;
+    float vx, vy;
+    float ax, ay;
+
     public:
-    Sprite(PrimordialGame *newG, string fname, int newX, int newY) {
-        xdir = 1;
-        ydir = 1;
-        good = true;
+    Sprite(PrimordialGame *newG, string fname,
+      float newX, float newY /*pixles*/, float newVx=1.0,
+      float newVy=1.0/*pixles per sec*/, 
+      float newAx=0.0, /* pixles per sec per sec*/
+      float newAy=1.0) {
         g = newG;
-        SDL_Surface *bmp = SDL_LoadBMP("test.bmp");
-        if (!bmp) { // bit map file
-            cerr << "Error reading bit map" << endl;
-            good = false;
-            return;
-        }
-        tex = SDL_CreateTextureFromSurface(g->getRenderer(),bmp); // convert the image to the texture
-        SDL_FreeSurface(bmp); // free up the memory of the surface in the cpu (pushed it to the graphics card in line above)
+        vx = newVx; // velocity This is like dt (can't integrate to get close, you can't control dt)
+        vy = newVy; // velocity 
+        good = true;
+        if (mm == NULL) good=false;
+        tex = mm->get(fname); // make use of MediaManager class to reference existing texture
+        if (tex == NULL) good=false;
         src.x=0; src.y=0; src.w=20; src.h=20;
         dest=src;
-        dest.x=newX; dest.y=newY;
+        dest.x=newX; dest.y=newY; 
+        x = newX; y = newY; 
+        ax = newAx; ay = newAy; 
     }
     void draw() {
         if (good) SDL_RenderCopy(g->getRenderer(),tex,&src,&dest);
     }
-    void update() {
-        dest.x += xdir;
-        dest.y += ydir;
-        if (dest.x > g->getW() || dest.x < 0) xdir *= -1;
-        if (dest.y > g->getH() || dest.y < 0) ydir *= -1;
+    void update(float dt) {
+        updatePosition(dt);
+        dest.x=x; // convert the floats (used for integration) to ints so it can be placed on a pixel on the screen
+        dest.y=y;
+    }
+    virtual void updatePosition(float dt) { // double integration in 2D:
+        vx += ax*dt; // gravity
+        vy += ay*dt;
+        x += vx*dt;  // velocity
+        y += vy*dt;
+        if (x > g->getW() || x < 0) vx *= -1;
+        if (y > g->getH() || y < 0) vy *= -1;
     }
 
     ~Sprite() {
         SDL_DestroyTexture(tex);
     }
 };
+
+/*class NPC: public Sprite {
+    public:
+    NPC(): Sprite() {
+
+    }
+    void updatePosition(float dt) {
+        vx += ax*dt; // gravity
+        vy += ay*dt;
+        x += vx*dt;  // velocity
+        y += vy*dt;
+        if (x > g->getW() || x < 0) vx *= -1;
+        if (y > g->getH() || y < 0) vy *= -1;
+    }
+};*/
 
 class Game: public PrimordialGame { // concrete instance of PrimordialGame
     SDL_Renderer *ren;
@@ -76,6 +130,7 @@ class Game: public PrimordialGame { // concrete instance of PrimordialGame
             return;
         }
         ren = SDL_CreateRenderer(window,-1,0); // my connection to my graphics card (needs to know what window to draw to)
+        mm = new MediaManager(ren); // hand the MediaManager the needed renderer so it can create and store the texture
     }
     void add(Sprite *s) { sprites.push_back(s); }
     const int getW() { return w; }
@@ -85,12 +140,18 @@ class Game: public PrimordialGame { // concrete instance of PrimordialGame
     }
     virtual void loop() {
         int count = 0;
-        while (count < 10000) {
+        int lastTime = SDL_GetTicks();
+        int firstTime = lastTime;
+        float dt;
+        while (lastTime - firstTime < 20000) { //go for two seconds (on everyone's machine)
             SDL_PumpEvents();
             SDL_RenderClear(ren);
             for (auto s:sprites) s->draw();
             SDL_RenderPresent(ren);
-            for (auto s:sprites) s->update();
+            int newTime = SDL_GetTicks();
+            dt = (float)(newTime - lastTime) / 1000.0; // how much time has passed in this function (in sec).
+            lastTime = newTime; // reset the time
+            for (auto s:sprites) s->update(dt);
             count++;
         }
     }
@@ -105,10 +166,14 @@ Game *g;
 int main() {
     Sprite *logo;
     g = new Game();
-    for (int i=0; i<10; i++) {
-        int x = rand() % g ->getW();
-        int y = rand() % g->getH();
-        logo = new Sprite(g,"test.bmp", 10,10);
+    for (int i=0; i<100; i++) { // Max: 66997
+        //float x = rand() % g ->getW();
+        //float y = rand() % g->getH();
+        float x = g->getW()/2;
+        float y = g->getH()/2;
+        float dx = rand() % 40 - 20; // randomize the velocities
+        float dy = rand() % 40 - 20;
+        logo = new Sprite(g,"test.bmp", x,y, dx, dy, 0.0, 10.0);
         g->add(logo);
     }
     g->loop();
